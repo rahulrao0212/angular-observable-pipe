f3968df46eae496ab599c91951d9c3cff4997d61
Rahul Rao <RAOR378647@exactsoftware.com>
2020-12-13 19:36:06 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable, pipe } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-observable-pipe';
+  obs = new Observable((observer) => {
+    observer.next(1)
+    observer.next(2)
+    observer.next(3)
+    observer.next(4)
+    observer.next(5)
+    observer.complete()
+  }).pipe(
+    tap(data => console.log('tap ' + data)),
+    filter(data => data > 2),
+    tap(data => console.log('tap filter ' + data)),                    //filter Operator
+    map((val) => { return val as number * 2 }),
+    tap(data => console.log('tap map ' + data)),  //map operator
+  )
+
+  ngOnInit() {
+    this.obs.subscribe(
+      val => {
+        console.log(val)
+      }
+    )
+    console.log('----------------------------------------')
+    this.customOperator(this.obs1).subscribe();
+  }
+
+  customOperator = pipe(
+    tap(data => console.log('tap ' + data)),
+    filter(data => data > 2),
+    tap(data => console.log('tap filter ' + data)),                    //filter Operator
+    map((val) => { return val as number * 2 }),
+    tap(data => console.log('tap map ' + data)),  //map operator
+  );
+
+  obs1 = new Observable((observer) => {
+    observer.next(1)
+    observer.next(2)
+    observer.next(3)
+    observer.next(4)
+    observer.next(5)
+    observer.complete()
+  })
 }
